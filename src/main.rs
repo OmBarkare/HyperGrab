24068176c05a9f1962bc0db6e88df0117e9208d5
OmBarkare <om062006@gmail.com>
2025-08-20 03:02:52 +0530

    feat: add file download
    Download any file from url. It will be saved in root
    directory. Files can be downloaded only if the url
    contains file name.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
+use std::env;
+use std::task::Context;
+use url::Url;
 use reqwest;
 
 
 fn main() -> Result<(), Box<dyn Error>>{
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let input_url = &args[1];
+    let parsed_url = Url::parse(input_url).unwrap();
+    let filename;
+    
+    if let Some(file_name) = parsed_url.path_segments().and_then(|s| s.last()) {
+        println!("{}", file_name);
+        filename = file_name;
+    } else {
+        panic!("Could Not Get file_name");
+    }
 
-    let resp = reqwest::blocking::get("https://www.rust-lang.org")?;
-    let body = resp.text()?;
-    let mut file = File::create("foo.txt")?;
-    file.write(body.as_bytes())?;
-    println!("{}", body);
+    let resp = reqwest::blocking::get(parsed_url.as_str())?;
+    let body = resp.bytes()?;
 
+    let mut file = File::create(filename)?;
+    file.write(&body[..])?;
+    
     Ok(())
 }
