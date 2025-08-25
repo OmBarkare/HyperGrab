a79cd057f5b547f788dffff28da8d7c368778cb2
OmBarkare <om062006@gmail.com>
2025-08-26 02:12:08 +0530

    feat: add function to get headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
 pub mod odm {
     use reqwest::header::{ACCEPT, HeaderValue, USER_AGENT};
     use std::env;
-    use std::net::TcpListener;
     use std::{fs::File, io::Write};
     use url::Url;
 
-    pub fn make_request(url: Url) -> reqwest::blocking::Response {
+    fn make_request(url: Url) -> reqwest::blocking::Response {
         let mut http_request_builder = reqwest::blocking::ClientBuilder::new();
         let mut headers = reqwest::header::HeaderMap::new();
         headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0"));
@@ -18,8 +17,8 @@ pub mod odm {
         // headers.insert(ACCEPT_ENCODING, HeaderValue::from_static("gzip, deflate"));
         // headers.insert(CONNECTION, HeaderValue::from_static("keep-alive"));
         http_request_builder = http_request_builder.default_headers(headers);
-        let http_request = http_request_builder.build().unwrap();
-        let resp = http_request.get(url.as_str()).send().unwrap();
+        let client = http_request_builder.build().unwrap();
+        let resp = client.get(url.as_str()).send().unwrap();
 
         resp
     }
@@ -29,6 +28,39 @@ pub mod odm {
             Ok(filename.to_string())
         } else {
             panic!("No filename fonud in url");
+        }
+    }
+
+    pub struct FileInfo {
+        pub size: u64,
+        pub supports_ranges: bool,
+    }
+
+    pub fn get_file_info(url: &Url) -> Result<FileInfo, anyhow::Error> {
+        let mut http_request_builder = reqwest::blocking::ClientBuilder::new();
+        let mut headers = reqwest::header::HeaderMap::new();
+        headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0"));
+        headers.insert(ACCEPT, HeaderValue::from_static("*/*"));
+
+        http_request_builder = http_request_builder.default_headers(headers);
+        let client = http_request_builder.build()?;
+
+        let resp = client.head(url.as_str()).send()?;
+
+        if !resp.status().is_success() {
+            anyhow::bail!("Server responded with error {}", resp.status());
+        } else {
+            let headers = resp.headers();
+            let content_len:u64 = headers.get("CONTENT_LENGTH").and_then(|v| v.to_str().ok()).and_then(|v| v.trim().parse().ok()).unwrap_or(0);
+
+            let supports_ranges = headers.get("ACCEPT_RANGES").unwrap().to_str().unwrap();
+            let supports_ranges = if supports_ranges == "bytes" {true} else {false};
+
+            if content_len == 0 {
+                anyhow::bail!("content length is zero");
+            }
+
+            Ok(FileInfo { size: content_len, supports_ranges: supports_ranges })
         }
     }
 
@@ -75,7 +107,6 @@ pub mod dmserver {
     }
 
     pub fn handle_request(listener: TcpListener) -> Result<String, ()> {
-
         for stream in listener.incoming() {
             let mut stream = stream.unwrap();
             let mut buffer = vec![0u8; 2048];
