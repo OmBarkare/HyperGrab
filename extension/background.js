cc964c9a05f05042239557a7ebf2db09518fc77d
OmBarkare <om062006@gmail.com>
2025-08-27 04:11:50 +0530

    feat: send chrome request header to request
    also did some refactoring in lob.rs

--- extension/background.js
+++ extension/background.js
@@ -1,17 +1,38 @@
 console.log("Extension Is Running");
+let allHeaders = {};
 
-setTimeout(() => {
-    chrome.downloads.onCreated.addListener((downloadItem) => {
+
+chrome.downloads.onCreated.addListener((downloadItem) => {
+    console.log("making post request");
+    let header = allHeaders[downloadItem.url];
     fetch("http://127.0.0.1:7878", {
         method: "POST",
         headers: {
             "Content-Type": "text/plain"
         },
-        body: downloadItem.url
+        body: JSON.stringify({
+            "url": downloadItem.url,
+            "headers": header
+        })
     });
-    console.log("Download detected: ", downloadItem.url);
-    chrome.downloads.cancel(downloadItem.id);
-    console.log("download forwarded to odm");
+    console.log("url", downloadItem.url);
+    console.log("headers: ", JSON.stringify(header, null, 2));
 })
-},1000);
 
+handler = function (details) {
+    let header = {};
+    for (const h of details.requestHeaders) {
+        header[h.name] = h.value;
+    }
+    allHeaders[details.url] = header;
+    console.log("adding to allHeaders: ", details.url);
+    console.log("header: ", JSON.stringify(header, null, 2));
+}
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    (details) => {
+        handler(details);
+    },
+    { urls: ["<all_urls>"] },
+    ["requestHeaders"]
+);
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,31 @@
 pub mod odm {
-    use reqwest::header::{ACCEPT, HeaderValue, USER_AGENT};
+    use anyhow::Ok;
+    use reqwest::header::{HeaderValue, ACCEPT, REFERER, USER_AGENT};
+    use reqwest::blocking::Client;
     use std::env;
     use std::{fs::File, io::Write};
     use url::Url;
 
-    fn make_request(url: Url) -> reqwest::blocking::Response {
-        let mut http_request_builder = reqwest::blocking::ClientBuilder::new();
+    fn make_http_client() -> Result<reqwest::blocking::Client, anyhow::Error> {
         let mut headers = reqwest::header::HeaderMap::new();
-        headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0"));
+        headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"));
         headers.insert(ACCEPT, HeaderValue::from_static("*/*"));
+        headers.insert(REFERER, HeaderValue::from_static("https://pixabay.com/"));
 
-        // let mut headers = HeaderMap::new();
-        // headers.insert(USER_AGENT, HeaderValue::from_static("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"));
-        // headers.insert(ACCEPT, HeaderValue::from_static("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"));
-        // headers.insert(ACCEPT_LANGUAGE, HeaderValue::from_static("en-US,en;q=0.9"));
-        // headers.insert(ACCEPT_ENCODING, HeaderValue::from_static("gzip, deflate"));
-        // headers.insert(CONNECTION, HeaderValue::from_static("keep-alive"));
-        http_request_builder = http_request_builder.default_headers(headers);
-        let client = http_request_builder.build().unwrap();
+        let client = reqwest::blocking::Client::builder().default_headers(headers).build()?;
+
+        Ok(client)
+    }
+
+    fn make_request(url: Url,client: Client) -> reqwest::blocking::Response {
         let resp = client.get(url.as_str()).send().unwrap();
 
         resp
     }
 
-    fn get_filename(url: &Url) -> Result<String, ()> {
+    fn get_filename(url: &Url) -> Result<String, anyhow::Error> {
         if let Some(filename) = url.path_segments().and_then(|s| s.last()) {
-            Ok(filename.to_string())
+            return Ok(filename.to_string())
         } else {
             panic!("No filename fonud in url");
         }
@@ -66,12 +66,10 @@ pub mod odm {
     }
 
     pub fn dowload_from_url_to(url: &Url, file: &mut File) {
-        let body_bytes = make_request(url.clone()).bytes().unwrap();
+        let client = make_http_client().unwrap();
+        let body_bytes = make_request(url.clone(), client).bytes().unwrap();
 
-        match file.write_all(&body_bytes[..]) {
-            Ok(_) => (),
-            Err(_) => panic!("Could Not write to file"),
-        }
+        file.write_all(&body_bytes[..]).unwrap();
     }
 
     pub fn get_path(url: &Url) -> String {
