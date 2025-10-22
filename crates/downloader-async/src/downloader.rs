a132772cc7eac31a22639815bc8e400a843ea0d6
OmBarkare <om062006@gmail.com>
2025-10-22 13:17:12 +0530

    feat: async download works
    Add a spawn_download_tasks function. Create sepereate
    task for each download

--- crates/downloader-async/src/downloader.rs
+++ crates/downloader-async/src/downloader.rs
@@ -1,14 +1,15 @@
 use std::{collections::HashMap, fmt::format, str::FromStr};
 
-use futures::StreamExt;
-use reqwest::{ self, header::{ HeaderMap, HeaderName, HeaderValue }, Client, ClientBuilder, Response };
+use futures::{future::join_all, StreamExt};
+use reqwest::{ self, header::{ self, HeaderMap, HeaderName, HeaderValue }, Client, ClientBuilder, Response };
 use tokio::{fs::{File, OpenOptions}, io::{AsyncSeekExt, AsyncWriteExt}};
 
 use crate::server_task::DownloadRequest;
 
-struct FileInfo {
-    content_length: u64,
-    accept_ranges: bool,
+pub struct FileInfo {
+    pub content_length: u64,
+    pub accept_ranges: bool,
+    pub file_name: String,
 }
 
 pub fn make_default_client(header_hashmap: &HashMap<String, String>) -> Client {
@@ -24,15 +25,66 @@ pub fn make_default_client(header_hashmap: &HashMap<String, String>) -> Client {
     client_builder.default_headers(head_map).build().unwrap()
 }
 
+pub async fn spawn_download_tasks(client: Client, url: &str, file_info: FileInfo, file_path: &str, chunks: u64) {
+    let file = File::create(file_path).await.unwrap();
+    file.set_len(file_info.content_length).await.unwrap();
+    drop(file);
+    let mut handles = Vec::new();
+    for i in 0..chunks {
+
+        let client = client.clone();
+        let url = url.to_string();
+        let file_path = file_path.to_string();
+        let content_length = file_info.content_length;
+
+        println!("Spawning task no {}", i);
+        let handle = tokio::spawn(async move {
+            let start = i * content_length / chunks;
+            let end = 
+            if i < (chunks - 1) {
+                (i + 1) * content_length / chunks - 1
+            } else {
+                content_length - 1
+            };
+
+            let mut file = OpenOptions::new().write(true).open(file_path).await.unwrap();
+            file.seek(std::io::SeekFrom::Start(start)).await.unwrap();
+
+            let range = format!("bytes={}-{}", start, end);
+            println!("range-{}=>{}-{}", i, start, end);
+            let resp = client.get(url).header("Range", range).send().await.unwrap();
+            let mut bytes_stream = resp.bytes_stream();
+
+            while let Some(bytes) = bytes_stream.next().await {
+                let bytes = bytes.unwrap();
+
+                file.write_all(&bytes).await.unwrap();
+            }
+        });
+        println!("Done pawning task no {}", i);
+        handles.push(handle);
+    }
+
+    println!("Joining all Handles !!");
+    join_all(handles).await;
+    println!("Done Downloading");
+}
+
 pub async fn get_file_info(client: Client, url: &str) -> Result<FileInfo, anyhow::Error> {
     let resp = client.head(url).send().await.unwrap();
     println!("{:?}", resp);
-    let content_length = resp.content_length().unwrap();
+    let content_length: u64 = resp.headers().get("content-length").unwrap().to_str().unwrap().parse().unwrap();
+    println!("CONTENT_LENGTH (inside get_file_info): {}",content_length);
 
     let accept_ranges = match resp.headers().get("accept-ranges") {
         Some(val) => val.to_str().unwrap_or("").eq_ignore_ascii_case("bytes"),
         _ => false,
     };
 
-    Ok(FileInfo { content_length, accept_ranges })
+    let file_name = resp.headers().get("content-disposition").unwrap()
+                                        .to_str()
+                                        .unwrap().split("filename=").nth(1).unwrap()
+                                        .trim_matches('"').trim_matches(';').to_string();
+
+    Ok(FileInfo { content_length, accept_ranges, file_name })
 }
--- crates/downloader-async/src/main.rs
+++ crates/downloader-async/src/main.rs
@@ -1,7 +1,6 @@
-use std::fs::{File, OpenOptions};
-
 use tokio::sync::mpsc;
-use crate::downloader::{make_default_client, get_file_info};
+use crate::{downloader::{get_file_info, make_default_client, spawn_download_tasks}, server_task::DownloadRequest};
+use dirs;
 
 mod server_task;
 mod downloader;
@@ -9,11 +8,22 @@ mod downloader;
 #[tokio::main]
 async fn main() {
     println!("Hello async world !");
-    
+    const CHUNKS: u64 = 6;
+
     let (tx, mut rx) = mpsc::channel(16);
     let server_handle = tokio::spawn(async move {
         server_task::start_listening("127.0.0.1:7878", tx).await.unwrap();
     });
+    let downloades_dir = dirs::download_dir().unwrap();
+    let downloades_dir = downloades_dir.to_str().unwrap();
+    while let Some(res) = rx.recv().await {
+        let def_client = make_default_client(&res.headers);
+        let file_info = get_file_info(def_client.clone(), &res.url).await.unwrap();
+        println!("CONTENT_LENGTH (in main): {}",file_info.content_length);
+        let file_path = format!("{}/{}", downloades_dir, file_info.file_name);
+
+        spawn_download_tasks(def_client.clone(), &res.url, file_info, &file_path, CHUNKS).await;
+    }
 
     server_handle.await.unwrap();
 }
