a51eff73ff8d93ebcaea9ab417c5ffc17265e0af
OmBarkare <om062006@gmail.com>
2025-10-23 23:32:47 +0530

    chore: add explainatory comments

--- crates/downloader-async/src/downloader.rs
+++ crates/downloader-async/src/downloader.rs
@@ -4,73 +4,16 @@ use futures::{future::join_all, StreamExt};
 use reqwest::{ self, header::{HeaderMap, HeaderName, HeaderValue }, Client, ClientBuilder };
 use tokio::{fs::{File, OpenOptions}, io::{AsyncSeekExt, AsyncWriteExt}};
 
+/// A struct to store info we get from a head request
+/// currently, it is assumed that the server accepts ranges so there is no
+/// fallback if the server doesnt, so the accept_ranges field isnt used
 pub struct FileInfo {
     pub content_length: u64,
     pub accept_ranges: bool,
     pub file_name: String,
 }
 
-pub fn make_default_client(header_hashmap: &HashMap<String, String>) -> Client {
-    let mut head_map = HeaderMap::new();
-    let client_builder = ClientBuilder::new();
-    for (key, val) in header_hashmap {
-        let name = HeaderName::from_str(&key).unwrap();
-        let value = HeaderValue::from_str(&val).unwrap();
-
-        head_map.insert(name, value);
-    }
-
-    client_builder.default_headers(head_map).build().unwrap()
-}
-
-pub async fn spawn_download_tasks(client: Client, url: &str, file_info: FileInfo, file_path: &str, chunks: u64) {
-    let file = File::create(file_path).await.unwrap();
-    file.set_len(file_info.content_length).await.unwrap();
-    drop(file);
-    let mut handles = Vec::new();
-    for i in 0..chunks {
-
-        let client = client.clone();
-        let url = url.to_string();
-        let file_path = file_path.to_string();
-        let content_length = file_info.content_length;
-
-        println!("Spawning task no {}", i);
-        let handle = tokio::spawn(async move {
-            let start = i * content_length / chunks;
-            let end = 
-            if i < (chunks - 1) {
-                (i + 1) * content_length / chunks - 1
-            } else {
-                content_length - 1
-            };
-
-            let mut file = OpenOptions::new().write(true).open(file_path).await.unwrap();
-            file.seek(std::io::SeekFrom::Start(start)).await.unwrap();
-            // let part_path = format!("{file_path}.part-{i}");
-            file.set_len(end - start + 1).await.unwrap();
-            let range = format!("bytes={}-{}", start, end);
-            println!("range-{}=>{}-{}", i, start, end);
-            let resp = client.get(url).header("Range", range).send().await.unwrap();
-            let mut bytes_stream = resp.bytes_stream();
-
-            let mut downloaded_bytes: u64 = 0;
-            while let Some(bytes) = bytes_stream.next().await {
-                let bytes = bytes.unwrap();
-                downloaded_bytes += bytes.len() as u64;
-                file.write_all(&bytes).await.unwrap();
-            }
-            println!("downloaded {downloaded_bytes} for task no {i}");
-        });
-        println!("Done pawning task no {}", i);
-        handles.push(handle);
-    }
-
-    println!("Joining all Handles !!");
-    join_all(handles).await;
-    println!("Done Downloading");
-}
-
+/// Function that sends a head requests and returns FileInfo.
 pub async fn get_file_info(client: Client, url: &str) -> Result<FileInfo, anyhow::Error> {
     let resp = client.head(url).send().await.unwrap();
     println!("{:?}", resp);
@@ -88,4 +31,74 @@ pub async fn get_file_info(client: Client, url: &str) -> Result<FileInfo, anyhow
                                         .trim_matches('"').trim_matches(';').to_string();
 
     Ok(FileInfo { content_length, accept_ranges, file_name })
+}
+
+/// makes and returns a client with headers that you pass as a
+/// hasmap
+pub fn make_default_client(header_hashmap: &HashMap<String, String>) -> Client {
+    let mut head_map = HeaderMap::new();
+    let client_builder = ClientBuilder::new();
+    for (key, val) in header_hashmap {
+        let name = HeaderName::from_str(&key).unwrap();
+        let value = HeaderValue::from_str(&val).unwrap();
+
+        head_map.insert(name, value);
+    }
+
+    client_builder.default_headers(head_map).build().unwrap()
+}
+
+/// this is the main function that does downloading
+/// Currently it does everyhting, calcualates ranges for chunks, spawns tasks
+/// for each chunk, downloads chunks, and writes them to the file.
+/// This needs seperation of concerns maybe ?
+pub async fn spawn_download_tasks(client: Client, url: &str, file_info: FileInfo, file_path: &str, chunks: u64) {
+    // creating file and setting length to content_length 
+    let file = File::create(file_path).await.unwrap();
+    file.set_len(file_info.content_length).await.unwrap();
+    drop(file);
+    let mut handles = Vec::new();
+    for i in 0..chunks {
+        // cloning all required fields to be used in a task
+        let client = client.clone();
+        let url = url.to_string();
+        let file_path = file_path.to_string();
+        let content_length = file_info.content_length;
+
+        println!("Spawning task no {}", i);
+        let handle = tokio::spawn(async move {
+            // calculating chunk range
+            let start = i * content_length / chunks;
+            let end = 
+            if i < (chunks - 1) {
+                (i + 1) * content_length / chunks - 1
+            } else {
+                content_length - 1
+            };
+
+            // opening file and seeking to starting byte of that range
+            let mut file = OpenOptions::new().write(true).open(file_path).await.unwrap();
+            file.seek(std::io::SeekFrom::Start(start)).await.unwrap();
+            file.set_len(end - start + 1).await.unwrap();
+            let range = format!("bytes={}-{}", start, end);
+            println!("range-{}=>{}-{}", i, start, end);
+            let resp = client.get(url).header("Range", range).send().await.unwrap();
+            let mut bytes_stream = resp.bytes_stream();
+            // keeping track of downloaded bytes for progress bar(#TODO) and to check if that chunk
+            // was downloaded fully
+            let mut downloaded_bytes: u64 = 0;
+            while let Some(bytes) = bytes_stream.next().await {
+                let bytes = bytes.unwrap();
+                downloaded_bytes += bytes.len() as u64;
+                file.write_all(&bytes).await.unwrap();
+            }
+            println!("downloaded {downloaded_bytes} for task no {i}");
+        });
+        println!("Done pawning task no {}", i);
+        handles.push(handle);
+    }
+
+    println!("Joining all Handles...");
+    join_all(handles).await;
+    println!("Done Downloading");
 }
--- crates/downloader-async/src/server_task.rs
+++ crates/downloader-async/src/server_task.rs
@@ -6,16 +6,23 @@ use anyhow::Error;
 use tokio::sync::mpsc;
 use tokio::net::TcpListener;
 
-
+/// struct to store the information sent to our localhost
+/// by our extension. Stores the download url as String
+/// and the headers as HashMap
 #[derive(Deserialize, Debug, Clone)]
 pub struct DownloadRequest {
     pub url: String,
     pub headers: HashMap<String, String>,   
 }
 
+/// A mpsc::Sender struct which we will wrap in arc so that we can 
+/// share this with the handler function, which cannot have arguments
+/// other than extractors
 pub struct AppState {
     tx: mpsc::Sender<DownloadRequest>,
 }
+
+
 pub async fn start_listening(addr: &str, tx: mpsc::Sender<DownloadRequest>) -> Result<(), Error> {
     let state = Arc::new(AppState{tx});
     let app = Router::new().route("/", post(handler)).with_state(state);
